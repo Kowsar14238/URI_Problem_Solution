27195737d000cfce8d0ef480c81df379ae6f2aa0
Mohammad Kowsar <88027531+Kowsar14238@users.noreply.github.com>
2023-09-14 00:11:51 +0600

    Add files via upload

---
+++ URI_Soluation_1080.cpp
@@ -0,0 +1,14 @@
+/// Find the highest value and its position from 100 numbers
+//URI 1080
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int input,i;
+
+    for(i=0; i<100;i++){
+        cin>> input
+    }
+
+}
