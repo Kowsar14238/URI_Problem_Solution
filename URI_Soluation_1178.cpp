202d358886d392a644d2dd0a81c107d3b7ac4c11
Mohammad Kowsar <88027531+Kowsar14238@users.noreply.github.com>
2023-09-29 08:43:16 +0600

    Add files via upload

---
+++ URI_Soluation_1178.cpp
@@ -0,0 +1,23 @@
+///Initial array N[100] and print subsequent position (1 up to 99) put half of the number inserted at the previous position.
+
+//#include<bits/stdc++.h>
+#include<iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+
+    double inputNumber, arr[100];
+
+    cin>> inputNumber;
+
+    for(int i=0; i<100; i++){
+
+        cout<< fixed<< setprecision(4)<< "N["<< i<< "] = "<< inputNumber<< endl;
+        inputNumber /= 2.0;
+
+    }
+
+    return 0;
+}
